c51eb9832aaf38d2ca02a9e4586861e093f2e054
Bryce <bryce@airpowerusa.com>
2025-08-20 09:36:03 -0700

    Add ESLint fix for apostrophes - ready for meeting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@next/next/no-img-element": "off"
+      "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off"
     }
   }
 ];
